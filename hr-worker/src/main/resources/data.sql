670d7931b83c034cf0f9a07a733e321b67f5a4b0
Renan Fernandes da Silva <64764009+RenanFernandesdev@users.noreply.github.com>
2023-07-22 01:50:51 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Incoming) VALUES ('Bob', 200.0);
+INSERT INTO tb_worker (name, daily_Incoming) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker (name, daily_Incoming) VALUES ('Alex', 250.0);
